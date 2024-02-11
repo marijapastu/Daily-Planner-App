fcb1f2ae20e9625d83ffd45b061efc512b8a1bce
Marija <marijap@mail.ru>
2024-02-11 21:32:20 +0000

    added JS code to save notes into local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,19 @@
 var currentTime = dayjs();
 $('#currentDay').text(currentTime.format('dddd[,] MMMM D'));
 
+// setting the local storage
+
+$(document).ready(function() {
+
+    function setNotes() {
+    $('.saveBtn').on('click', function(event) {
+    
+        var textId = $(this).siblings('textarea').attr('id');
+        var notes = $(this).siblings('textarea').val();
+        localStorage.setItem(textId, notes);
+    
+        });
+    
+    }
+    setNotes();
+})   
